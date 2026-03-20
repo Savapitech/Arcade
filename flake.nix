1df967f9ee50c627fb95796aa4e9454e2f01c399
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-03-20 15:17:41 +0100

    Fix flake

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
       nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
+        "aarch64-darwin"
       ] (system: function nixpkgs.legacyPackages.${system});
   in {
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         hardeningDisable = ["fortify"];
         packages = with pkgs; [
-          valgrind
           compiledb
           gcovr
           criterion
