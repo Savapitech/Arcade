0592af0356c01ad1768440ce54bb63da9467c2aa
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-03-22 21:04:57 +0100

    Fix entity constructor

--- src/Game/Entity.cpp
+++ src/Game/Entity.cpp
@@ -1,11 +1,11 @@
-#include "Entity.hpp"
-
 #include <cstddef>
 #include <string>
 
+#include "Entity.hpp"
+
 namespace game {
-Entity::Entity(const std::string name, const std::string path,
-               const std::string pathCli, arc::Vec2 coord, arc::Vec2 boxSize)
-    : _name(name), _path(path), _pathCli(pathCli), _startCoord(x, y),
-      _curCoord(x, y) {}
+Entity::Entity(const std::string name, const std::string texturePath,
+               const std::string asciiTexture, arc::Vec2 pos, arc::Vec2 boxSize)
+    : _name(name), _texturePath(texturePath), _asciiTexture(asciiTexture), _startPos(pos),
+      _pos(pos) {}
 } // namespace game
