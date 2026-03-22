f8cefd4a8fce91494422aff5b950abd50ff11cb5
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-03-22 23:49:25 +0100

    Add DLLoader

--- src/Core/Loader.hpp
+++ src/Core/Loader.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
-#include <memory>
+#include <dlfcn.h>
+#include <string>
 
-#include "Game.hpp"
-
-namespace core {
-class DLLoader {
+template <typename T> class DLLoader {
 public:
-  DLLoader();
+  DLLoader(const std::string &path) : _handle(nullptr), _path(path) {
+    _handle = dlopen(_path.c_str(), RTLD_LAZY);
+
+    if (_handle == nullptr)
+      throw std::runtime_error(dlerror());
+  }
+
+  ~DLLoader() {
+    if (_handle != nullptr)
+      dlclose(_handle);
+  }
+
+  T *getInstance(const std::string &symbol) {
+    dlerror();
+
+    using create_t = T *(*)();
+
+    create_t entryPoint =
+        reinterpret_cast<create_t>(dlsym(_handle, symbol.c_str()));
+
+    const char *err = dlerror();
+    if (err)
+      throw std::runtime_error(err);
+
+    return entryPoint();
+  }
+
+private:
+  void *_handle;
+  std::string _path;
 };
-} // namespace core
