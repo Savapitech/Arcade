66c8caa6a0a3b5bdc0606e517c6f27841262b92a
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-03-22 23:49:31 +0100

    Up entities

--- src/Graphic/Graphic.hpp
+++ src/Graphic/Graphic.hpp
@@ -2,16 +2,16 @@
 
 #include <vector>
 
-#include "Entity.hpp"
+#include "../Game/Entity.hpp"
 
 namespace graphic {
-class IGraphic {
-public:
-  virtual void openWindow() = 0;
-  virtual void closeWindow() = 0;
-  virtual bool isOpen() = 0;
+  class IGraphic {
+    public:
+      virtual void openWindow(size_t heigth, size_t width, const std::string &windowName) = 0;
+      virtual void closeWindow() = 0;
+      virtual bool isOpen() = 0;
 
-  virtual void initWindow(const std::vector<game::Entity> &) = 0;
-  virtual void drawEntities(const std::vector<game::Entity> &) = 0;
-};
+      virtual void initGraphic(const std::vector<game::Entity> &) = 0;
+      virtual void drawEntities(const std::vector<game::Entity> &) = 0;
+  };
 } // namespace graphic
