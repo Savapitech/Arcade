1b1eeaef053aa901cc41be9738161a31321e10c3
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-03-23 11:47:37 +0100

    Fix event

--- src/Core/Event.hpp
+++ src/Core/Event.hpp
@@ -1,17 +1,30 @@
 #pragma once
 
-#include "Keys.hpp"
-
 #include <stack>
+
+#include "Keys.hpp"
+#include "Utils.hpp"
+
+
+/*Macro non-blocking or blocking mouse update*/
+#define MNOBLOCK    (1 >> 1)
+#define MNBLOCK     (1 >> 2)
 
 class Event
 {
     private:
+        core::Vec2 mouseCoord;
         std::stack<core::Keys> keyStack;
         bool close = false;
     public:
+        const core::Vec2 getMouseCoord();
+        void updateMouseCoord(core::Vec2);
+        bool isClicked();
+        bool updateClicked();
+        bool MouseUpdateMode(uint8_t);
         bool getCloseState(){return close;}
         void setCloseState(bool state){close = state;}
         std::stack<core::Keys> &getKeyStack(){return keyStack;}
         void addKey(core::Keys key){keyStack.push(key);}
+
 };
